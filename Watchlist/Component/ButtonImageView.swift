c5deb74fb5a5e686af3242947d504b8d4db21326
Kanika Arora <kanikaarora5@outlook.com>
2025-02-26 15:13:36 +0530

    Watchlist: The prototype for adding movies in a list is made. Its working. Now you can add your favourite movies along with its genre.

---
+++ Watchlist/Component/ButtonImageView.swift
@@ -0,0 +1,29 @@
+//
+//  ButtonImageView.swift
+//  Watchlist
+//
+//  Created by kanika on 26/02/25.
+//
+
+import SwiftUI
+
+struct ButtonImageView: View {
+    let symbolName: String
+    
+    var body: some View {
+        Image(systemName: symbolName)
+            .resizable()
+            .scaledToFit()
+            .foregroundStyle(.blue.gradient)
+            .padding(8)
+            .background(
+                Circle()
+                    .fill(.ultraThickMaterial)
+            )
+            .frame(width: 80)
+    }
+}
+
+#Preview {
+    ButtonImageView(symbolName: "plus.circle.fill")
+}
--- Watchlist/View/ContentView.swift
+++ Watchlist/View/ContentView.swift
@@ -14,17 +14,54 @@ struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @Query private var movies: [Movie]
     
+    @State private var isSheetPresented: Bool = false
+    
     
     var body: some View {
         List {
-            
+            ForEach(movies){ movie in
+                HStack{
+                    Text(movie.title)
+                    
+                    Spacer()
+                    
+                    Text(movie.genre.name)
+                }//: LIST ROW
+                .swipeActions {
+                    Button(role: .destructive){
+                        withAnimation{
+                            modelContext.delete(movie)
+                        }
+                    }label: {
+                        Label("Delete", systemImage: "trash")
+                    }
+                }
+            }
         }//: LIST
         .overlay{
             if movies.isEmpty{
                 EmptyListView()
             }
         }
+        //MARK: - SAFE AREA
+        .safeAreaInset(edge: .bottom, alignment: .center){
+            //NEW MOVIE BUTTON
+            Button{
+                isSheetPresented.toggle()
+            } label: {
+                ButtonImageView(symbolName: "plus.circle.fill")
+            }
+        } //: SAFE ATEA
+        // MARK:- SHEET
+        .sheet(isPresented: $isSheetPresented){
+            NewMovieFormView()
+        }
     }
+}
+
+#Preview("Sample Data"){
+    ContentView()
+        .modelContainer(Movie.preview)
 }
 
 #Preview("Empty List") {
