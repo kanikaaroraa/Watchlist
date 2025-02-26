e86c2811b05084261cd2ff907cda6ee85bcd9ca7
Kanika Arora <kanikaarora5@outlook.com>
2025-02-26 11:38:58 +0530

    Watchlist: Initialized iOS App

---
+++ Watchlist/WatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchlistApp.swift
+//  Watchlist
+//
+//  Created by kanika on 26/02/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
